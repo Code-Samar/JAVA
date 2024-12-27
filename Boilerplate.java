507e1468923379b9440acc1affab1f334373d445
Samar Ahmad <codewithsamsol@gmail.com>
2024-12-27 23:55:51 +0530

    Create Boilerplate.java

---
+++ Boilerplate.java
@@ -0,0 +1,7 @@
+public class Boilerplate {
+    public static void main(String[] args) {
+        System.out.println("This is a boilerplate output in Java!");
+        // code
+        // code
+    }
+}
