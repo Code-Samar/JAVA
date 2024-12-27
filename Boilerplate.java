31aa0f866176c96af6ae0f54363181e0c5c10739
Samar Ahmad <codewithsamsol@gmail.com>
2024-12-27 23:58:02 +0530

    Delete Boilerplate.java

--- Boilerplate.java
+++
@@ -1,7 +0,0 @@
-public class Boilerplate {
-    public static void main(String[] args) {
-        System.out.println("This is a boilerplate output in Java!");
-        // code
-        // code
-    }
-}
